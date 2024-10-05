462068c6fdbce0b1faf34da51a54daf3bfc76d4d
Salem388 <salemmohamed7300@gmail.com>
2024-10-05 09:55:16 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        Coded 
+        by{" "}
+        <a
+          className="footer--link underline-animation"
+          href="https://github.com/Salem388"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Salem Mohamed
+        </a>
+      </small>
+    </footer>
+  );
+}
